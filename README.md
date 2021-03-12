241220d1c7c968d192ece82e71745e7ffb3a811a
Paul R. Tagliamonte <paultag@gmail.com>
2021-03-12 09:40:27 -0500

    Fix typos using misspell

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # hz.tools/rf
 
-Package rf contains go fundemental types and helpers to allow Go code to
+Package rf contains go fundamental types and helpers to allow Go code to
 easily manupulate RF related types.
--- doc.go
+++ doc.go
@@ -18,7 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE. }}}
 
-// Package rf contains go fundemental types and helpers to allow Go code to
+// Package rf contains go fundamental types and helpers to allow Go code to
 // easily manupulate RF related types.
 //
 // Additional types and helpers can be found in `hz.tools/sdr` for dealing
